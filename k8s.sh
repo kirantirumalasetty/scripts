323385540ab23da198f3e4ae1c7edc7c7cd1a834
kiran <kirantwls@gmail.com>
2024-10-26 11:15:54 +0530

    script

---
+++ k8s.sh
@@ -0,0 +1,15 @@
+echo "Installing KUBECTL"
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+echo " Version of ths kubetcl is : $(kubectl version)"
+
+echo "EKSCTL installation"
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
+
