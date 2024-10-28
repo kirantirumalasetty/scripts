ff9afde25dd4ca9731bd19874107108d53feda2d
kiran <kirantwls@gmail.com>
2024-10-28 08:33:33 +0530

    scripts

--- disk-resize.sh
+++ disk-resize.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 lsblk
-sudo growpart /dev/nvme0n1 4
+AA=$(lsblk -d -o NAME /dev/nvme[01]n1|tail -1)
+echo "S{AA}"
+sudo growpart /dev/${AA} 4
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol
 sudo lvextend -l +50%FREE /dev/RootVG/varVol
 sudo xfs_growfs /
